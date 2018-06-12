ac1aa9fb80eef21c75659a704e52b6561f8c3ce9
Jason <Jason@USCSGANA1L1C.corp.emc.com>
2018-06-12 15:07:48 -0700

    configured all changes needed to connect to oracle db.

--- CPSC304/src/package1/Main.java
+++ CPSC304/src/package1/Main.java
@@ -3,8 +3,8 @@ package package1;
 import java.sql.*;
 
 public class Main {
-	static String username = "user";
-	static String password = "password";
+	static String username = "ora_s1h0b";
+	static String password = "a33731143";
 	public static void main(String[] args) throws Exception {
 		//loads driver 
                        
@@ -12,18 +12,19 @@ public class Main {
 		
 		//creates connection to ugrad db
 		Connection con = DriverManager.getConnection(
-				  "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", 
+				  "jdbc:oracle:thin:@localhost:1522:ug", 
 				username, password);
 		
 		//make a sql statement
 		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM branch");
 		
-		//while(rs.next()){
-			System.out.println(rs);
-		//}
-			//
+		while(rs.next()){
+			System.out.println(rs.getString(1));
+		}
 		
+		
+			
 		rs.close();
 		stmt.close();
 		con.close();
