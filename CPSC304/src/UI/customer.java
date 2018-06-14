2bc6da7a72e8dc9222df43ae90389ce4c3929204
yuan xu <a2589654@gmail.com>
2018-06-14 16:24:22 -0700

    Add files via upload

---
+++ CPSC304/src/UI/customer.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class customer {
+    private JTextArea text_proname;
+    private JTextArea text_proprice;
+    private JTextArea text_probrand;
+    private JTextArea text_proID;
+    private JTextArea text_sellerID;
+    private JTextArea text_sellername;
+    private JTextField price_low;
+    private JTextField price_high;
+    private JTextField search_proID;
+    private JButton searchProductsByNameButton;
+    private JButton searchProductsByPriceButton;
+    private JButton findCheapestProductButton;
+    private JButton findMostExpensiveProductButton;
+    private JTextField rate_proID;
+    private JTextField rate_score;
+    private JButton rateByIDButton;
+    private JTextField order_proID;
+    private JTextField order_sellerID;
+    private JTextField order_quantity;
+    private JButton putOrderButton;
+}
