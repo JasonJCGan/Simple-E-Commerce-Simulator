2e02ece233d964dfb2eb9edca5ade2d251ee2158
jason <jason_gan96@hotmail.com>
2018-06-16 17:16:40 -0700

    Changes to connect, tested and works

--- CPSC304/src/package1/Connections.java
+++ CPSC304/src/package1/Connections.java
@@ -5,12 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connections {
+    static String connectURL = "jdbc:oracle:thin:@localhost:1522:ug";
+    static String username = "ora_s1h0b";
+    static String password = "a33731143";
     private static Connection con=null;
 
     private Connections() throws SQLException{
-        con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522/ug", "ora_s1h0b", "a33731143");
-        //con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522/ug", "ora_p6g1b", "a18564104");
-
+        try {
+            // Load the Oracle JDBC driver
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            con = DriverManager.getConnection(connectURL,username,password);
+            System.out.println("\nConnected to Oracle!");
+        }
+        catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+        }
     }
 
     public static Connection getConnection() throws SQLException{
