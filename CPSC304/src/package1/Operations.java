08b8a8ae9f99fbdd5dc1cda9ded592de966fdfd3
yuan xu <a2589654@gmail.com>
2018-06-16 17:15:17 -0700

    Add files via upload

--- CPSC304/src/package1/Operations.java
+++ CPSC304/src/package1/Operations.java
@@ -1,4 +1,45 @@
 package package1;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Operations {
+    /**************** Queries for Customer Login(YUAN) *****************************/
+
+    public boolean c_login(String id,Connection con) throws SQLException {
+        boolean r;
+        try{
+            int c_id = Integer.parseInt(id);
+            try (PreparedStatement ps = con.prepareStatement
+                    ("SELECT Customer_ID FROM Customer WHERE Customer_ID = ? ")) {
+                ps.setInt(1, c_id);
+                ResultSet temp = ps.executeQuery();
+                r = !temp.wasNull();
+                ps.close();
+            }
+        }catch (Exception e){
+            throw new SQLException();
+        }
+    return r;
+    }
+
+    /**************** Queries for Seller Login(YUAN) *****************************/
+    public boolean s_login(String id,Connection con) throws SQLException {
+        boolean r;
+        try{
+            int s_id = Integer.parseInt(id);
+            try (PreparedStatement ps = con.prepareStatement
+                    ("SELECT Seller_ID FROM Seller WHERE Seller_ID = ? ")) {
+                ps.setInt(1, s_id);
+                ResultSet temp = ps.executeQuery();
+                r = !temp.wasNull();
+                ps.close();
+            }
+        }catch (Exception e){
+            throw new SQLException();
+        }
+        return r;
+    }
 }
