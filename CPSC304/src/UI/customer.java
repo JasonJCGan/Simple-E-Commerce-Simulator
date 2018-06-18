9bb768051be42ff93fbe118fba41eff8d8f1b271
Justin Tang <justin.tang@alumni.ubc.ca>
2018-06-17 17:20:05 -0700

    Additions to Customer

--- CPSC304/src/UI/customer.java
+++ CPSC304/src/UI/customer.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.sql.*;
+import package1.Connections;
+
 public class customer {
     private JTextArea text_proname;
     private JTextArea text_proprice;
@@ -11,9 +14,6 @@ public class customer {
     private JTextArea text_sellername;
     private JTextField price_low;
     private JTextField price_high;
-    private JTextField search_proID;
-    private JButton searchProductsByNameButton;
-    private JButton searchProductsByPriceButton;
     private JButton findCheapestProductButton;
     private JButton findMostExpensiveProductButton;
     private JTextField rate_proID;
@@ -24,6 +24,11 @@ public class customer {
     private JTextField order_quantity;
     private JButton putOrderButton;
     public JPanel CustomerUI;
+    private JTextField search_proName;
+    private JTextField search_sellerName;
+    private JButton searchButton;
+    private JTextField search_rating;
+    private JButton findRatingButton;
     private String customerID;
 
     public void setCustomerID(String customerID) {
@@ -31,28 +36,134 @@ public class customer {
     }
 
     public customer() {
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String SELECT = "SELECT P.producthas_id";
+                String FROM = " FROM producthas P ";
+                String WHERE = "";
+                try {
+                    Connection con = Connections.getConnection();
+                    Statement stmt = con.createStatement();
+                    ResultSet rs;
+                    String proName = search_proName.getText();
+                    String low = price_low.getText();
+                    String high = price_high.getText();
+                    String sellerName = search_sellerName.getText();
+
+                    WHERE = getLikePName(WHERE, proName);
+                    WHERE = setPriceRange(WHERE, low, high);
+
+                    if (!search_sellerName.getText().isEmpty()){
+                        SELECT += ", seller S";
+                        if (WHERE.isEmpty()) {
+                            WHERE = "WHERE P.seller_id = S.seller_id AND S.seller_name LIKE %" + search_sellerName + "%";
+                        }
+                        else {
+                            WHERE = WHERE + " AND P.seller_id = S.seller_id AND S.seller_name LIKE %" + search_sellerName + "%";
+                        }
+                    }
+
+                    rs =stmt.executeQuery(SELECT + FROM + WHERE);
+                    // Do something with ResultSet
+
+                }catch(SQLException ex){
+                    System.out.println("Search failed (Combo): " + ex.getMessage());
+                }
+            }
+
+            private String getLikePName(String where, String proName) {
+                if (!proName.isEmpty()){
+                    if (where.isEmpty()) {
+                        where = "WHERE P.producthas_name LIKE %" + proName + "%";
+                    }
+                    else {
+                        where += " AND P.producthas_name LIKE %" + proName + "%";
+                    }
+                }
+                return where;
+            }
+
+            private String setPriceRange(String where, String low, String high) {
+                if (!(low.isEmpty() && high.isEmpty())){
+                    if (where.isEmpty()) {
+                        where = "WHERE P.producthas_price <= " + high + " AND P.producthas_price >= " + low;
+                    }
+                    else {
+                        where += " AND P.producthas_price <= " + high + " AND P.producthas_price >= " + low;
+                    }
+                }
+                else if (!low.isEmpty()){
+                    if (where.isEmpty()) {
+                        where = "WHERE P.producthas_price >= " + low;
+                    }
+                    else {
+                        where += " AND P.producthas_price >= " + low;
+                    }
+                }
+                else if (!high.isEmpty()) {
+                    if (where.isEmpty()) {
+                        where = "WHERE P.producthas_price <= " + high;
+                    }
+                    else {
+                        where += " AND P.producthas_price >= " + high;
+                    }
+                }
+                return where;
+            }
+        });
+
+        findRatingButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection con = Connections.getConnection();
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT MIN(P.producthas_price) " +
+                            "FROM producthas P " +
+                            "WHERE P.producthas_id = " + search_rating);
+
+                    // Do something with ResultSet
+
+                }
+                catch (SQLException ex){
+                    System.out.println("Search failed (Rating): " + ex.getMessage());
+                }
+           }
+        });
+
         findCheapestProductButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection con = Connections.getConnection();
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT MIN(P.producthas_price) " +
+                            "FROM producthas P");
 
+                    // Do something with ResultSet
+
+                }
+                catch (SQLException ex){
+                    System.out.println("Search failed (Rating): " + ex.getMessage());
+                }
             }
         });
         findMostExpensiveProductButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection con = Connections.getConnection();
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT MAX(P.producthas_price) " +
+                            "FROM producthas P");
 
-            }
-        });
-        searchProductsByNameButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+                    // Do something with ResultSet
 
-            }
-        });
-        searchProductsByPriceButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+                }
+                catch (SQLException ex){
+                    System.out.println("Search failed (Rating): " + ex.getMessage());
+                }
             }
         });
         rateByIDButton.addActionListener(new ActionListener() {
@@ -67,5 +178,8 @@ public class customer {
 
             }
         });
+
+
+
     }
 }
