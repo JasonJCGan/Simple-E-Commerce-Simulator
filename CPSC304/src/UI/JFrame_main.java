105660c98b3101e8273567c8b4ca6183fff49552
jason <jason_gan96@hotmail.com>
2018-06-18 00:36:08 -0700

    fixed login logic, made connection a static block in class.

--- CPSC304/src/UI/JFrame_main.java
+++ CPSC304/src/UI/JFrame_main.java
@@ -1,21 +1,13 @@
-import package1.Connections;
-
 import javax.swing.*;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class JFrame_main {
     public static void main(String[] args) throws SQLException {
-        Connection con = Connections.getConnection();
-
         javax.swing.JFrame frame = JFrames.get_frame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Login");
         frame.setContentPane(new login().log_in);
         frame.pack();
         frame.setVisible(true);
-
-
-
     }
 }
